ecaf88f8450168a9e82bd8cda62f18fda0c48a0e
NullifiedVlad <alpatof74@gmail.com>
2020-05-18 00:51:53 +0300

    update

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def joke(ctx):
 
 @bot.command()
 async def steam(ctx, url):
-    await ctx.message.delete()
+    # await ctx.message.delete()
 
     heads = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0'}
     r = requests.get(url, headers=heads)
