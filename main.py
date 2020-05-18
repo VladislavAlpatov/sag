78a2fc79db34dc48443ae576036e9a8df817e465
NullifiedVlad <alpatof74@gmail.com>
2020-05-18 15:03:10 +0300

    fix

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ async def joke(ctx):
 
 
 @bot.command()
-@bot.command()
 async def steam(ctx, url):
 
     heads = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0'}
