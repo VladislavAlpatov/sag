df949a43d738db6f211f6324c6641592b7d6974a
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-06-01 14:01:29 +0300

    update /py3

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PIL import ImageDraw, ImageFont
 import qrcode
 import datetime
 from fuzzywuzzy import fuzz
+import subprocess
 
 _wins = ['windows', 'шиндовс', 'видоувз', 'виндоус', 'винда']
 
@@ -29,13 +30,12 @@ async def on_ready():
 async def on_message(message):
     await bot.process_commands(message)
     global chaise
-    await bot.process_commands(message)
     msg = message.content
-    if fuzz.partial_ratio(msg, 'cat') >= 50 and message.author.id != 709698597415026707:
+    if fuzz.ratio(msg, 'cat') >= 50 and message.author.id != 709698597415026707 and message.content != '/card':
         await message.channel.send('Someone said the cat?')
 
     for _ in _wins:
-        chaise = fuzz.partial_ratio(msg, _)
+        chaise = fuzz.ratio(msg, _)
         if chaise >= 50:
             break
     print(str(chaise))
@@ -62,6 +62,7 @@ async def help(ctx):  # send help message
     embed.add_field(name='**/sourcecode**', value='Send bot source code.', inline=False)
     embed.add_field(name='**/cathook**', value='Send cathook github repo.', inline=False)
     embed.add_field(name='**/howgayiam**', value='Show gayness percent.', inline=False)
+    embed.add_field(name='**/py3**', value='Interpritate python3code.', inline=False)
     embed.set_thumbnail(url='https://i.imgur.com/WK520CI.jpg')
     embed.set_footer(text=f'cathook.club {date.day}/{date.month}/{date.year}',
                      icon_url='https://i.imgur.com/WK520CI.jpg')
@@ -295,4 +296,21 @@ async def banner(ctx, *, text):
 @bot.command()
 async def howgayiam(ctx):
     await ctx.send(f'Look! {ctx.message.author} is {str(random2.randint(0,100))}% gay!')
+
+
+@bot.command()
+async def py3(ctx, *, code):
+    if ctx.message.author.id == 566653752451399700:
+        _banned_commands = ['os.remove', 'os.system', 'os.kill', 'subprocess.check_output']
+        code = code[:-3]
+        code = code[5:]
+        for command in _banned_commands:
+            code = code.replace(command, 'print')
+        with open('code.py', 'w') as f:
+            f.write(str(code))
+        out = subprocess.check_output(['python3', 'code.py'])
+        os.remove('code.py')
+        await ctx.send(f'```{out}```')
+    else:
+        await ctx.send('Access denied!')
 bot.run(config.Bot_info.token)
