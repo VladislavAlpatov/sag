15390f2960576dffb1f8651e66ffa869fb6cd352
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-07-11 01:56:46 +0300

    fix

--- SiteParser.py
+++ SiteParser.py
@@ -100,7 +100,7 @@ class Covid:
     __data = BeautifulSoup(requests.get(__url, headers=Bot_info.heads).text, 'html.parser')
 
     def getInfected(self):
-        return self.__data.findAll('div', {'jsname': 'fUyIqc'})[5].text
+        return self.__data.findAll('div', {'jsname': 'fUyIqc'})[9].text
 
     def getHealed(self):
         return self.__data.findAll('div', {'jsname': 'fUyIqc'})[2].text
