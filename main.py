b54d0897b67befc1980cb2ecc812e9bd89bd502d
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-08-26 22:33:57 +0300

    card fixes

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ from discord.ext import commands
 import asyncio
 import config
 
-bot = commands.Bot(command_prefix='/')  # префикс для комманд
+bot = commands.Bot(command_prefix='c!')  # префикс для комманд
 bot.remove_command('help')
+print(bot.command_prefix)
 
 
 @bot.event
@@ -45,22 +46,22 @@ async def help_message(ctx):  # send help message
     date = datetime.datetime.now()
     embed = discord.Embed(title='**FEATURES**', description='Discord cathook bot.', color=0x0095ff, )
     # заголовки
-    embed.add_field(name='**/help**', value='Send this message.', inline=False)
-    embed.add_field(name='**/cat**', value='Send random cat image.', inline=False)
-    embed.add_field(name='**/feature**', value='Random cathook feature.', inline=False)
-    embed.add_field(name='**/joke**', value='Send joke.', inline=False)
-    embed.add_field(name='**/steam**', value='Check steam profile.', inline=False)
-    embed.add_field(name='**/card**', value='Send your profile card.', inline=False)
-    embed.add_field(name='**/invite**', value='Send bot invitation.', inline=False)
-    embed.add_field(name='**/nigga**', value='Make nigga meme.', inline=False)
-    embed.add_field(name='**/qr**', value='Make qrcode.', inline=False)
-    embed.add_field(name='**/online**', value='Show TF2 online stats.', inline=False)
-    embed.add_field(name='**/cathook**', value='Send cathook github repo.', inline=False)
-    embed.add_field(name='**/howgayami**', value='Show gayness percent.', inline=False)
-    embed.add_field(name='**/why**', value='Another russian meme.', inline=False)
-    embed.add_field(name='**/howfurryami**', value='Show furry percent.', inline=False)
-    embed.add_field(name='**/think**', value='Make russian meme.', inline=False)
-    embed.add_field(name='**/covid**', value='Show covid stats.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}help**', value='Send this message.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}cat**', value='Send random cat image.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}feature**', value='Random cathook feature.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}joke**', value='Send joke.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}steam**', value='Check steam profile.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}card**', value='Send your profile card.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}invite**', value='Send bot invitation.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}nigga**', value='Make nigga meme.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}qr**', value='Make qrcode.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}online**', value='Show TF2 online stats.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}cathook**', value='Send cathook github repo.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}howgayami**', value='Show gayness percent.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}why**', value='Another russian meme.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}howfurryami**', value='Show furry percent.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}think**', value='Make russian meme.', inline=False)
+    embed.add_field(name=f'**{bot.command_prefix}covid**', value='Show covid stats.', inline=False)
     embed.set_thumbnail(url='https://i.imgur.com/WK520CI.jpg')
     embed.set_footer(text=f'cathook.club {date.day}/{date.month}/{date.year}',
                      icon_url='https://i.imgur.com/WK520CI.jpg')
@@ -112,7 +113,11 @@ async def steam(ctx, url_custom):
     embed.set_footer(text=url_custom,
                      icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/512px'
                               '-Steam_icon_logo.svg.png')
-    await ctx.message.delete()
+    try:
+        await ctx.message.delete()
+    except discord.errors.Forbidden:
+        pass
+
     await ctx.send(embed=embed)
 
 
@@ -183,23 +188,20 @@ async def card(ctx):
                 avatar = avatar.resize((164, 164), Image.ANTIALIAS)
                 self.body.paste(avatar, (27, 34))
 
-        def drawNick(self):
+        def build(self):
+            # ник
             font = ImageFont.truetype(self.font, 50, encoding="unic")
             self.draw.text((207, 18), self.author.name, font=font)
-
-        def drawTeg(self):
+            # тег
             font = ImageFont.truetype(self.font, 30, encoding="unic")
             self.draw.text((207, 78), 'TAG: #' + str(self.author.discriminator), font=font)
-
-        def drawID(self):
+            # id
             font = ImageFont.truetype(self.font, 25, encoding="unic")
             self.draw.text((207, 118), 'ID: ' + str(self.author.id), font=font)
-
-        def drawServer(self):
+            # серер
             font = ImageFont.truetype(self.font, 25, encoding="unic")
             self.draw.text((207, 150), 'SERVER: ' + self.guild, font=font)
-
-        def creatorChecker(self):
+            # метка создателя
             if self.author.id == 566653752451399700:
                 with Image.open('media/card/developer_ico.png') as avatar:
                     avatar = avatar.resize((100, 100), Image.ANTIALIAS)
@@ -208,10 +210,8 @@ async def card(ctx):
             else:
                 pass
 
-            # сохраняем и отправляем карточку
-
-        def build(self, name: str):
-            self.body.save(name)
+            # сохраняем
+            self.body.save('card.jpg')
 
         @staticmethod
         def cleanFiles():
@@ -222,22 +222,17 @@ async def card(ctx):
     user = Card('media/fonts/sans.ttf',
                 'media/card/steam_background.jpg')
 
-    user.addAvatar()
-    user.drawNick()
-    user.drawID()
-    user.drawTeg()
-    user.drawServer()
-    user.creatorChecker()
-    user.build('card.jpg')
+    user.build()
     await ctx.send(file=discord.File('card.jpg'))
     user.cleanFiles()
-    print(str(user))
     del user
 
 
 @bot.command()
 async def invite(ctx):
-    await ctx.send(config.Messages.invite)
+    await ctx.send('''
+    **You can add me on your server by this link:**
+`https://discord.com/api/oauth2/authorize?client_id=709698597415026707&permissions=387136&scope=bot`''')
 
 
 @bot.command()
@@ -289,31 +284,6 @@ async def qr(ctx, *, text):
         os.remove('code.png')
     else:
         await ctx.send('You cant create qrcode with 100 symbols or more!')
-
-
-@bot.command()
-async def banner(ctx, *, text):
-    symbols = int(len(text))
-    if symbols < 30:
-
-        if 1 <= symbols < 12:
-            y = 30
-        elif 12 <= symbols < 24:
-            y = 37
-        else:
-            y = 15
-        image = Image.new('RGB', ((symbols * y), 70), (59, 196, 255))
-        draw = ImageDraw.Draw(image)
-        font_name = 'media/fonts/arialbd.ttf'
-        large = 40
-        font = ImageFont.truetype(font_name, large, encoding="unic")
-        draw.text((10, 20), str(text), font=font)
-
-        image.save('banner.jpg')
-        await ctx.send(file=discord.File('banner.jpg'))
-        os.remove('banner.jpg')
-    else:
-        await ctx.send('You cant create banner with 30 symbols or more!')
 
 
 @bot.command()
@@ -425,4 +395,4 @@ async def tf2stats(ctx):
     img.cleanfiles()
 
 
-bot.run(config.Bot_info.token)
+bot.run(config.Bot_info.token, bot=False)
