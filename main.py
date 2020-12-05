59f781d73e30c44ff7e7bf54d0e4dc24a05a83dc
VladislavAlpatov <alpatof74@gmail.com>
2020-12-05 23:06:47 +0300

    code refactoring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+# created by NullifiedVlad 2020.
 import datetime
 import os
-import SiteParser
+from modules import SiteParser
 import discord
 import qrcode
 import random
@@ -12,10 +13,14 @@ import markovify
 
 
 class Cat(commands.Bot):
-    def __init__(self, command_prefix, token: str, **options):
+    """
+    bot: False - official bot , True - self bot
+    """
+    def __init__(self, command_prefix, token: str, bot: bool = False, **options):
         super().__init__(command_prefix, **options)
         self.remove_command('help')
         self.__token = token
+        self.bot = bot
 
     @staticmethod
     def __sentence(file: str):
@@ -39,11 +44,6 @@ class Cat(commands.Bot):
         async def on_message(message):
             await self.process_commands(message)
 
-        @self.event
-        async def on_member_join(member):
-            # выдача ролец для своего сервер (Hacker space)
-            print(member)
-
         @self.command(aliases=['help'])
         async def help_message(ctx):  # send help message
             date = datetime.datetime.now()
@@ -60,9 +60,7 @@ class Cat(commands.Bot):
             embed.add_field(name=f'**{self.command_prefix}online**', value='Show TF2 online stats.', inline=False)
             embed.add_field(name=f'**{self.command_prefix}cathook**', value='Send cathook github repo.', inline=False)
             embed.add_field(name=f'**{self.command_prefix}howgayami**', value='Show gayness percent.', inline=False)
-            embed.add_field(name=f'**{self.command_prefix}why**', value='Another russian meme.', inline=False)
             embed.add_field(name=f'**{self.command_prefix}howfurryami**', value='Show furry percent.', inline=False)
-            embed.add_field(name=f'**{self.command_prefix}think**', value='Make russian meme.', inline=False)
             embed.set_thumbnail(url='https://i.imgur.com/WK520CI.jpg')
             embed.set_footer(text=f'cathook.club {date.day}/{date.month}/{date.year}',
                              icon_url='https://i.imgur.com/WK520CI.jpg')
@@ -71,7 +69,9 @@ class Cat(commands.Bot):
 
         @self.command()
         async def cat(ctx):
-            image = requests.get('https://thiscatdoesnotexist.com/', headers=config.Bot_info.heads)
+            image = requests.get('https://thiscatdoesnotexist.com/',
+                                 headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
+                                                        'rv:75.0) Gecko/20100101 Firefox/75.0'})
             with open('cat.jpg', 'wb') as f:
                 f.write(image.content)
             await ctx.send(file=discord.File('cat.jpg'))
@@ -87,10 +87,12 @@ class Cat(commands.Bot):
 
         @self.command()
         async def joke(ctx):
-            url = requests.get('https://jokes.lol/random-jokes/', headers=config.Bot_info.heads)
+            url = requests.get('https://jokes.lol/random-jokes/',
+                               headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; '
+                                                      'rv:75.0) Gecko/20100101 Firefox/75.0'})
             soup = BeautifulSoup(url.text, 'html.parser')
             soup = soup.find('div', {'class': 'query-field query-field-post_content'})
-            await ctx.send(soup.text)
+            await ctx.send(soup.text[:-15])
 
         @self.command()
         async def steam(ctx, url_custom):
@@ -107,7 +109,8 @@ class Cat(commands.Bot):
             embed.set_thumbnail(url=account.getProfilePicture())
             embed.set_author(name='Steam profile checker.', icon_url='https://i.imgur.com/WK520CI.jpg')
             embed.set_footer(text=url_custom,
-                             icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg/512px'
+                             icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Steam_icon_logo.svg'
+                                      '/512px '
                                       '-Steam_icon_logo.svg.png')
             try:
                 await ctx.message.delete()
@@ -136,35 +139,33 @@ class Cat(commands.Bot):
 
                 def addavatar(self):
                     avatar = str(ctx.author.avatar_url)
-                    img = requests.get(avatar, headers=config.Bot_info.heads)
+                    img = requests.get(avatar)
 
                     with open('ava.webp', 'wb') as f:
                         f.write(img.content)
 
                     with Image.open('ava.webp') as avatar:
-                        avatar = avatar.convert('RGB')
                         avatar = avatar.resize((164, 164), Image.ANTIALIAS)
                         self.body.paste(avatar, (27, 34))
 
                 def build(self):
-                    # ник
+                    # name
                     font = ImageFont.truetype(self.font, 50, encoding="unic")
-                    self.draw.text((207, 18), self.author.name, font=font)
-                    # тег
+                    self.draw.text((207, 18), ctx.message.author.name, font=font)
+                    # tag
                     font = ImageFont.truetype(self.font, 30, encoding="unic")
                     self.draw.text((207, 78), 'TAG: #' + str(self.author.discriminator), font=font)
                     # id
                     font = ImageFont.truetype(self.font, 25, encoding="unic")
                     self.draw.text((207, 118), 'ID: ' + str(self.author.id), font=font)
-                    # серер
+                    # server
                     font = ImageFont.truetype(self.font, 25, encoding="unic")
                     self.draw.text((207, 150), 'SERVER: ' + self.guild, font=font)
-                    # метка создателя
+                    # creator mark
                     if self.author.id == 566653752451399700:
                         with Image.open('media/card/developer_ico.png') as avatar:
-                            avatar = avatar.resize((100, 100), Image.ANTIALIAS)
-                            avatar.convert('RGB')
-                            self.body.paste(avatar, (573, 0), avatar)
+                            avatar = avatar.resize((50, 50), Image.ANTIALIAS)
+                            self.body.paste(avatar, (625, 0), avatar)
                     else:
                         pass
 
@@ -199,7 +200,7 @@ class Cat(commands.Bot):
         @self.command()
         async def qr(ctx, *, text):
             if int(len(text)) <= 100:
-                image = qrcode.make(str(text))
+                image = qrcode.make(text)
                 image.save('code.png')
 
                 await ctx.send(file=discord.File('code.png'))
@@ -216,51 +217,7 @@ class Cat(commands.Bot):
             if ctx.message.author.id == 566653752451399700:
                 await ctx.send(content=f'{ctx.message.author} is **100%** furry!')
             else:
-                await ctx.send(content=f'{ctx.message.author} is **{str(random.randint(0, 100))}%** furry!')
-
-        @self.command(aliases=['мысль', 'гигант'])
-        async def think(ctx):
-            url = ctx.message.attachments[0].url
-            r = requests.get(str(url))
-
-            with open('image.jpg', 'wb') as f:
-                f.write(r.content)
-
-            image = Image.open('media/think/гигант_мысли.jpg')
-            image_on_paste = Image.open('image.jpg')
-
-            image_on_paste = image_on_paste.resize((500, 400), Image.ANTIALIAS)
-            image.paste(image_on_paste, (130, 13))
-
-            image.save(f'{str(ctx.author.id)}.png')
-
-            await ctx.message.delete()
-            await ctx.send(file=discord.File(f'{str(ctx.author.id)}.png'))
-
-            os.remove(f'{str(ctx.author.id)}.png')
-            os.remove('image.jpg')
-
-        @self.command(aliases=['зачем', 'нахуя'])
-        async def why(ctx):
-            url = ctx.message.attachments[0].url
-            r = requests.get(str(url))
-
-            with open('image.jpg', 'wb') as f:
-                f.write(r.content)
-
-            image = Image.open('media/think/why.jpg')
-            image_on_paste = Image.open('image.jpg')
-
-            image_on_paste = image_on_paste.resize((614, 336), Image.ANTIALIAS)
-            image.paste(image_on_paste, (72, 42))
-
-            image.save(f'{str(ctx.author.id)}.png')
-
-            await ctx.message.delete()
-            await ctx.send(file=discord.File(f'{str(ctx.author.id)}.png'))
-
-            os.remove(f'{str(ctx.author.id)}.png')
-            os.remove('image.jpg')
+                await ctx.send(content=f'{ctx.message.author} is **{random.randint(0, 100)}%** furry!')
 
         @self.command(aliases=['stats', 'tf2', 'online'])
         async def tf2stats(ctx):
@@ -293,8 +250,8 @@ class Cat(commands.Bot):
             await ctx.send(file=discord.File(f'{str(ctx.author.id)}.png'))
             img.cleanfiles()
 
-        self.run(self.__token, bot=False)
+        self.run(self.__token, bot=self.bot)
 
 
 if __name__ == '__main__':
-    Cat('cat_', 'NzY3MDY4MDA1Nzk5OTUyMzg1.X8YcPA.eYn8HACFeR2fodTDZlUas31MtM8').start_bot()
+    Cat('cat_', 'NzY3MDY4MDA1Nzk5OTUyMzg1.X8YcPA.eYn8HACFeR2fodTDZlUas31MtM8', False).start_bot()
